672c66f794844688b7160857bac6fb76fc7287ef
Matúš Koterba <matus.koterba@gmail.com>
2025-12-29 18:42:35 +0100

    add histogram

---
+++ src/Aggregation/HistogramAggregation.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bonu\ElasticsearchBuilder\Aggregation;
+
+use Bonu\ElasticsearchBuilder\Exception\Aggregation\InvalidIntervalException;
+
+/**
+ * @see https://www.elastic.co/docs/reference/aggregations/search-aggregations-bucket-histogram-aggregation
+ */
+class HistogramAggregation implements AggregationInterface
+{
+    use FilterableAggregation;
+    use GlobalizableAggregation;
+
+    /**
+     * @param string|\Stringable $name
+     * @param string|\Stringable $field
+     * @param int|float $interval
+     * @param null|int $minDocCount
+     *
+     */
+    public function __construct(
+        protected string | \Stringable $name,
+        protected string | \Stringable $field,
+        protected int | float $interval = 10,
+        protected ?int $minDocCount = null
+    ) {
+        if ($interval < 1) {
+            throw new InvalidIntervalException('Interval must be a positive number. ' . $interval . ' given.');
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName(): string
+    {
+        return (string) $this->name;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toArray(): array
+    {
+        $value = ['histogram' => array_filter([
+            'field' => (string) $this->field,
+            'interval' => $this->interval,
+            'min_doc_count' => $this->minDocCount,
+        ], static fn (mixed $value): bool => $value !== null)];
+        $value = $this->addFilterToAggregation($value, $this->getName());
+        $value = $this->addGlobalToAggregation($value, $this->getName());
+
+        return [
+            $this->getName() => $value,
+        ];
+    }
+}
---
+++ tests/Unit/Aggregation/HistogramAggregationTest.php
@@ -0,0 +1,155 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bonu\ElasticsearchBuilder\Tests\Unit\Aggregation;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\Depends;
+use Bonu\ElasticsearchBuilder\Tests\TestCase;
+use PHPUnit\Framework\Attributes\DependsExternal;
+use Bonu\ElasticsearchBuilder\Aggregation\HistogramAggregation;
+use Bonu\ElasticsearchBuilder\Tests\Fixture\BoolQueryFixture;
+use Bonu\ElasticsearchBuilder\Exception\Aggregation\InvalidIntervalException;
+use Bonu\ElasticsearchBuilder\Tests\Unit\Aggregation\Trait\FilterableAggregationTest;
+use Bonu\ElasticsearchBuilder\Tests\Unit\Aggregation\Trait\GlobalizableAggregationTest;
+
+/**
+ * @internal
+ */
+final class HistogramAggregationTest extends TestCase
+{
+    #[Test]
+    public function itBuildsBasicHistogramAggregation(): void
+    {
+        $agg = new HistogramAggregation('prices', 'price');
+
+        $this->assertSame([
+            'prices' => [
+                'histogram' => [
+                    'field' => 'price',
+                    'interval' => 10,
+                ],
+            ],
+        ], $agg->toArray());
+    }
+
+    #[Depends('itBuildsBasicHistogramAggregation')]
+    #[Test]
+    public function itBuildsWithCustomIntervalAndMinDocCount(): void
+    {
+        $agg = new HistogramAggregation('prices', 'price', 50, 1);
+
+        $this->assertSame([
+            'prices' => [
+                'histogram' => [
+                    'field' => 'price',
+                    'interval' => 50,
+                    'min_doc_count' => 1,
+                ],
+            ],
+        ], $agg->toArray());
+    }
+
+    #[Test]
+    public function itThrowsExceptionForInvalidInterval(): void
+    {
+        $this->expectException(InvalidIntervalException::class);
+
+        new HistogramAggregation('prices', 'price', 0);
+    }
+
+    #[Test]
+    public function itThrowsExceptionForNegativeInterval(): void
+    {
+        $this->expectException(InvalidIntervalException::class);
+
+        new HistogramAggregation('prices', 'price', -5);
+    }
+
+    #[Depends('itBuildsBasicHistogramAggregation')]
+    #[DependsExternal(FilterableAggregationTest::class, 'itAddsFilterToAggregation')]
+    #[Test]
+    public function itCanBeFiltered(): void
+    {
+        $agg = new HistogramAggregation('prices', 'price');
+        $agg = $agg->query(new BoolQueryFixture('foo'));
+
+        $this->assertSame([
+            'prices' => [
+                'filter' => [
+                    'foo' => 'fixture_for_bool_query',
+                ],
+                'aggs' => [
+                    'prices' => [
+                        'histogram' => [
+                            'field' => 'price',
+                            'interval' => 10,
+                        ],
+                    ],
+                ],
+            ],
+        ], $agg->toArray());
+    }
+
+    #[Depends('itBuildsBasicHistogramAggregation')]
+    #[DependsExternal(GlobalizableAggregationTest::class, 'itAddsGlobalToAggregation')]
+    #[Test]
+    public function itCanBeGlobal(): void
+    {
+        $agg = new HistogramAggregation('prices', 'price');
+        $agg = $agg->asGlobal();
+
+        $this->assertEquals([
+            'prices' => [
+                'global' => (object) [],
+                'aggs' => [
+                    'prices' => [
+                        'histogram' => [
+                            'field' => 'price',
+                            'interval' => 10,
+                        ],
+                    ],
+                ],
+            ],
+        ], $agg->toArray());
+    }
+
+    #[Depends('itBuildsBasicHistogramAggregation')]
+    #[Depends('itCanBeFiltered')]
+    #[Depends('itCanBeGlobal')]
+    #[Test]
+    public function itCanBeGlobalAndFilteredTogether(): void
+    {
+        $agg = new HistogramAggregation('prices', 'price', 20);
+        $agg = $agg->asGlobal()->query(new BoolQueryFixture('foo'));
+
+        $this->assertEquals([
+            'prices' => [
+                'global' => (object) [],
+                'aggs' => [
+                    'prices' => [
+                        'filter' => [
+                            'foo' => 'fixture_for_bool_query',
+                        ],
+                        'aggs' => [
+                            'prices' => [
+                                'histogram' => [
+                                    'field' => 'price',
+                                    'interval' => 20,
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ], $agg->toArray());
+    }
+
+    #[Test]
+    public function itReturnsCorrectName(): void
+    {
+        $agg = new HistogramAggregation('prices', 'price');
+        $this->assertSame('prices', $agg->getName());
+    }
+}
