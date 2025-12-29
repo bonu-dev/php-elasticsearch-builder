53fb4c0f7811423fcca379c033a00c9542707341
Matúš Koterba <matus.koterba@gmail.com>
2025-12-29 19:46:26 +0100

    feat: Add histogram aggregation (#24)

---
+++ src/Aggregation/HistogramAggregation.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bonu\ElasticsearchBuilder\Aggregation;
+
+use Bonu\ElasticsearchBuilder\Exception\Aggregation\InvalidIntervalException;
+
+use function array_filter;
+
+/**
+ * @see https://www.elastic.co/docs/reference/aggregations/search-aggregations-bucket-histogram-aggregation
+ */
+class HistogramAggregation implements AggregationInterface
+{
+    use FilterableAggregation;
+    use GlobalizableAggregation;
+
+    /**
+     * @param string|\Stringable $name
+     * @param string|\Stringable $field
+     * @param float|int $interval
+     * @param null|int $minDocCount
+     *
+     * @throws \Bonu\ElasticsearchBuilder\Exception\Aggregation\InvalidIntervalException
+     */
+    public function __construct(
+        protected string | \Stringable $name,
+        protected string | \Stringable $field,
+        protected int | float $interval = 10,
+        protected ?int $minDocCount = null
+    ) {
+        if ($interval <= 0) {
+            throw new InvalidIntervalException('Interval must be a positive number. ' . $interval . ' given.');
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName(): string
+    {
+        return (string) $this->name;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toArray(): array
+    {
+        $value = ['histogram' => array_filter([
+            'field' => (string) $this->field,
+            'interval' => $this->interval,
+            'min_doc_count' => $this->minDocCount,
+        ], static fn (mixed $value): bool => $value !== null)];
+        $value = $this->addFilterToAggregation($value, $this->getName());
+        $value = $this->addGlobalToAggregation($value, $this->getName());
+
+        return [
+            $this->getName() => $value,
+        ];
+    }
+}
